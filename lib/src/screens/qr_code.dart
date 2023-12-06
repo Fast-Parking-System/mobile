5ee761f81538b9e727f0bf38cd9befdd5937298d
Naura <naurazp26@gmail.com>
2023-12-06 18:28:26 +0700

    render qr code

--- lib/src/screens/qr_code.dart
+++ lib/src/screens/qr_code.dart
@@ -1,19 +1,14 @@
 import 'dart:convert';
 
-import 'package:fast_parking_system/src/models/locations_model.dart';
-import 'package:fast_parking_system/src/models/pokemon_model.dart';
 import 'package:fast_parking_system/src/models/whoami_model.dart';
-import 'package:fast_parking_system/src/sample_feature/sample_item_list_view.dart';
 import 'package:fast_parking_system/src/screens/account.dart';
 import 'package:fast_parking_system/src/screens/home.dart';
 import 'package:fast_parking_system/src/screens/profile.dart';
 import 'package:fast_parking_system/src/screens/login.dart';
-import 'package:fast_parking_system/src/screens/qr_code.dart';
 import 'package:fast_parking_system/src/screens/wallet.dart';
 import 'package:fast_parking_system/src/services/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
 
 class QRCode extends StatefulWidget {
   const QRCode({Key? key}) : super(key: key);
@@ -26,8 +21,7 @@ class QRCode extends StatefulWidget {
 
 class _HomeState extends State<QRCode> {
   TextEditingController searchController = TextEditingController();
-  late Pokemon? _pokemon = null;
-  late WhoAmI? _whoami = null;
+  late WhoAmI? _whoami;
   final storage = const FlutterSecureStorage();
   int _selectedIndex = 2;
 
@@ -45,17 +39,10 @@ class _HomeState extends State<QRCode> {
   }
 
   Future<void> _readAll() async {
-    // Read value
     String? token = await storage.read(key: 'token');
     print('token:  $token');
-    // Read all values
     Map<String, String> allValues = await storage.readAll();
     print(allValues);
-    // setState(() {
-    //   _items = all.entries
-    //       .map((entry) => _SecItem(entry.key, entry.value))
-    //       .toList(growable: false);
-    // });
   }
 
   void _onItemTapped(int index) {
@@ -96,9 +83,6 @@ class _HomeState extends State<QRCode> {
             IconButton(
               icon: const Image(image: AssetImage('assets/images/logout.png')),
               onPressed: () async {
-                // Navigate to the logout page. If the user leaves and returns
-                // to the app after it has been killed while running in the
-                // background, the navigation stack is restored.
                 await storage.delete(key: 'token');
                 Navigator.restorablePushNamed(context, Login.routeName);
               },
@@ -131,29 +115,9 @@ class _HomeState extends State<QRCode> {
             ? const Center(
                 child: CircularProgressIndicator(),
               )
-            // : GridView.count(
-            //         crossAxisCount: 2,
-            //         children: List.generate(_whoami!.data.length, (index) {
-            //           return Column(
-            //             children: [
-            //               const Image(image: AssetImage('assets/images/location.png')),
-            //               Center(
-            //                 child: Text(
-            //                   _whoami!.data[index].name.toUpperCase(),
-            //                   style: Theme.of(context).textTheme.headlineSmall,
-            //                 ),
-            //               )
-            //             ],
-            //           );
-            //         }),
-            //       ),
             : Container(
-                // ... Your existing code ...
                 child: Column(
                   children: [
-                    // ... Your existing code ...
-
-                    // Add this part to display the base64 image
                     const SizedBox(height: 30),
                     _whoami!.data.qrCode != null
                         ? Image.memory(
@@ -162,8 +126,6 @@ class _HomeState extends State<QRCode> {
                             height: 200.0,
                           )
                         : const SizedBox.shrink(),
-
-                    // ... Your existing code ...
                   ],
                 ),
               ));
