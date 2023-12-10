af9ff708c2d66ced7b84a49fbee2f16217f00711
Halimah <nunurhalimah17@gmail.com>
2023-12-10 15:42:26 +0700

    fix qr disappear when on wide screen

--- lib/src/screens/qr_code.dart
+++ lib/src/screens/qr_code.dart
@@ -133,6 +133,7 @@ class _HomeState extends State<QRCode> {
                         Image.memory(
                           base64Decode(_whoami!.data.qrCode),
                           width: double.infinity,
+                          height: 400,
                         ),
                     ],
                   ),
